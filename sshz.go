a9b804978530b98f866b0ee9f71a631e4ff6eb53
Hugo Tavares Reis <hugo@linux.com>
2020-07-30 09:45:29 +0200

    defined cli

--- sshz.go
+++ sshz.go
@@ -7,14 +7,14 @@ import (
 )
 
 var cliArgs struct {
-	Verbose      bool     `kong:"help:'verbose logging' short:'v'"`
-	HostsList    string   `kong:"help:'file containing hosts list (or - to use stdin)' type:'existingfile' short:'l' default:'-'"`
-	SSHUsername  string   `kong:"help:'ssh username' short:'u' required"`
-	Concurrency  int      `kong:"help:'number of parallel connections' default:64"`
-	Timeout      int      `kong:"help:'connect timeout, in seconds' default:15"`
-	IgnoreStderr bool     `kong:"help:'ignore stderr'"`
-	OutputFormat string   `kong:"help:'output format: simple,extended or json' enum:'simple,extended,json' default:'simple'"`
-	Commands     []string `kong:"arg name:'commands'"`
+	Verbose      bool     `help:"verbose logging" short:"v"`
+	HostsList    string   `help:"file containing hosts list (or - to use stdin)" type:"existingfile" short:"l" default:"-"`
+	SSHUsername  string   `help:"ssh username" short:"u" required:"true"`
+	Concurrency  int      `help:"number of parallel connections" default:"64"`
+	Timeout      int      `help:"connect timeout, in seconds" default:"15"`
+	IgnoreStderr bool     `help:"ignore stderr"`
+	OutputFormat string   `help:"output format: simple,extended or json" enum:"simple,extended,json" default:"simple"`
+	Commands     []string `arg:"" name:"commands"`
 }
 
 func main() {
